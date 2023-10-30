4b4586094522118ef822adba3077a73e0bdd24e5
rcochin <romain.cochin.01@gmail.com>
2023-10-30 17:11:29 +0100

    add documentation

--- README.md
+++ README.md
@@ -1,3 +1,87 @@
-# simple-component-libraryA library of React components created using `create-react-app`.
+# `@r.cochin/modal-react` - A Simple React Modal Component Library
 
-## InstallationRun the following command:`npm install simple-component-library`
+A library of React components created using `create-react-app`.
+
+## Installation
+
+Run the following command:
+
+\`\`\`bash
+npm install "@r.cochin/modal-react"
+\`\`\`
+
+## Usage
+
+To use the `Modal` component in your React application, you can import it as shown below:
+
+\`\`\`jsx
+import Modal from '@r.cochin/modal-react';
+\`\`\`
+
+### Modal Component
+
+The `Modal` component is designed to be simple yet flexible. Here's an example of how to use it:
+
+\`\`\`jsx
+<Modal 
+  isOpen={true} 
+  autoClose={true} 
+  autoCloseDuration={3000} 
+  onClose={() => console.log("Modal Closed")} 
+  color="lightgray" 
+  border="10px"
+>
+  <h1>Hello, World!</h1>
+</Modal>
+\`\`\`
+
+#### Parameters
+
+Here are the props you can pass to the `Modal` component:
+
+- `isOpen` (boolean): Determines if the modal is open or closed. Default is `false`.
+
+- `autoClose` (boolean): Determines if the modal should automatically close after a certain duration. Default is `false`.
+
+- `autoCloseDuration` (number): Specifies the duration (in milliseconds) after which the modal should automatically close. Default is `3000` (3 seconds).
+
+- `onClose` (function): A callback function that is called when the modal is closed.
+
+- `color` (string): Specifies the background color of the modal. Default is `'white'`.
+
+- `border` (string): Specifies the border-radius of the modal. Default is `'5px'`.
+
+#### Callbacks
+
+- `onClose`: This function is called when the modal is closed. Useful for performing actions like resetting form states or logging.
+
+### Example
+
+Here's a more detailed example:
+
+\`\`\`jsx
+const App = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    console.log("Modal closed");
+  };
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      <Modal 
+        isOpen={isOpen} 
+        autoClose={true} 
+        autoCloseDuration={5000} 
+        onClose={handleClose}
+        color="lightblue"
+        border="10px"
+      >
+        <h1>Hello, World!</h1>
+      </Modal>
+    </div>
+  );
+};
+\`\`\`
--- modal-react/src/lib/components/Modal.js
+++ modal-react/src/lib/components/Modal.js
@@ -1,33 +1,52 @@
+// Import the necessary modules and assets
 import React, { useEffect } from 'react';
 import closeSVG from '../assets/close.png';
 import './Modal.css';
 
-
+// Define the Modal functional component
 function Modal(props) {
-    const { autoClose, autoCloseDuration, onClose } = props;
+  // Destructure the props for easier access
+  const { autoClose, autoCloseDuration, onClose } = props;
 
-    useEffect(() => {
-        if (autoClose) {
-            const timeout = setTimeout(() => {
-                onClose();
-            }, autoCloseDuration || 3000);
-    
-            return () => clearTimeout(timeout);
+  // Use the useEffect hook to run logic when the component mounts or updates
+  useEffect(() => {
+    // Check if the modal should auto-close
+    if (autoClose) {
+      // Set a timer to close the modal
+      const timeout = setTimeout(() => {
+        onClose(); // Call the onClose function after the specified duration
+      }, autoCloseDuration || 3000); // Use the duration from props, or default to 3 seconds
+
+      // Cleanup function to clear the timer when the component is unmounted
+      return () => clearTimeout(timeout);
+    }
+  }, [autoClose, autoCloseDuration, onClose]); // Declare dependencies for useEffect
+
+  // If the modal isn't open, don't render anything
+  if (!props.isOpen) return null;
+
+  // Render the modal
+  return (
+    <div className='modal-container'>
+      <div 
+        className='modal-content' 
+        style={{ 
+          backgroundColor: props.color || 'white', 
+          borderRadius: props.border || '5px' 
+        }}
+      >
+        {props.children} {/* Render the children passed to the Modal component */}
+        {
+          // If autoClose is false, render the close button
+          !props.autoClose && 
+          <button className='close-btn' onClick={props.onClose}>
+            <img className='close-img' src={closeSVG} alt='bouton de fermeture'/>
+          </button>
         }
-    }, [autoClose, autoCloseDuration, onClose]);
-    
-    if (!props.isOpen) return null;
-
-    return (
-        <div className='modal-container'>
-            <div className='modal-content' style={{ backgroundColor: props.color || 'white', borderRadius: props.border || '5px' }}>
-                {props.children}
-                {!props.autoClose && <button className='close-btn' onClick={props.onClose}><img className='close-img' src={closeSVG} alt='bouton de fermeture'/></button>}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
+// Export the Modal component for use in other files
 export default Modal;
-
-console.log('test');
